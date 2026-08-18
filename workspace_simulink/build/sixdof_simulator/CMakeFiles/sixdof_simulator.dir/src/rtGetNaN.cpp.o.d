c1e3b1f7e40d8d4ae07cf284b5b90f89fe950028
Sofia Vazquez Lapa Dias Duarte <sofia.vazquez.duarte@gmail.com>
2026-08-18 23:30:45 +0100

    Remove generated build artifacts from tracking and add ignore rules for build/install/log/cache files
    - Add .gitignore rules for clangd cache (.cache/), compile_commands.json, and *.o
    - Create workspace_simulink/.gitignore (build/, install/, log/)
    - Extend workspace_erocket/src/erocket/.gitignore (install/, log/)
    - Untrack 365 generated files: clangd index cache, colcon build/install/log output, compile_commands.json

--- workspace_simulink/build/sixdof_simulator/CMakeFiles/sixdof_simulator.dir/src/rtGetNaN.cpp.o.d
+++
@@ -1,9 +0,0 @@
-CMakeFiles/sixdof_simulator.dir/src/rtGetNaN.cpp.o: \
- /home/sofia/Documents/E-Rocket-All-Stuff/E-Rocket-flight-software/workspace_simulink/src/sixdof_simulator/src/rtGetNaN.cpp \
- /usr/include/stdc-predef.h \
- /home/sofia/Documents/E-Rocket-All-Stuff/E-Rocket-flight-software/workspace_simulink/src/sixdof_simulator/include/sixdof_simulator/rtwtypes.h \
- /home/sofia/Documents/E-Rocket-All-Stuff/E-Rocket-flight-software/workspace_simulink/src/sixdof_simulator/include/sixdof_simulator/rtGetNaN.h \
- /home/sofia/Documents/E-Rocket-All-Stuff/E-Rocket-flight-software/workspace_simulink/src/sixdof_simulator/include/sixdof_simulator/rt_nonfinite.h \
- /usr/lib/gcc/x86_64-linux-gnu/13/include/stddef.h \
- /home/sofia/Documents/E-Rocket-All-Stuff/E-Rocket-flight-software/workspace_simulink/src/sixdof_simulator/include/sixdof_simulator/rtwtypes.h \
- /home/sofia/Documents/E-Rocket-All-Stuff/E-Rocket-flight-software/workspace_simulink/src/sixdof_simulator/include/sixdof_simulator/rt_nonfinite.h
--- workspace_simulink/build/sixdof_simulator/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sofia/Documents/E-Rocket-All-Stuff/E-Rocket-flight-software/workspace_simulink/src/sixdof_simulator
-# Build directory: /home/sofia/Documents/E-Rocket-All-Stuff/E-Rocket-flight-software/workspace_simulink/build/sixdof_simulator
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
